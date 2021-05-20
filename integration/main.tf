e68cf05c6a91e15b59f3cdbac627e6f7305b35ee
Arun prasad Krishnaraj <34764@Hexaware.com>
2021-05-20 11:42:34 +0530

    updated azurerm backend

--- integration/main.tf
+++ integration/main.tf
@@ -2,6 +2,15 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    storage_account_name = "githubcicdtfstate"
+    container_name       = "integration"
+    key                  = "terraform.tfstate"
+    resource_group_name  = "cdsimplifiedv2"
+  }
+}
+
 module "vm" {
   source = "../modules/vm"
 }
